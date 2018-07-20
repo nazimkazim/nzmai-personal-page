37cce280b9b78f15d37c735b02490dd4efc93f9c
Your Name <nazik_111@mail.ru>
2018-07-20 18:18:52 +0500

    add post routes

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const passport = require('passport');
 
+// Post model
+const Post = require('../../models/Post');
+
+// Validation
+const validatePostInput = require('../../validation/post');
 
 // @route GET api/posts/test
 // @desc Test post route
@@ -8,5 +15,31 @@ const router = express.Router();
 router.get('/test', (req, res) => {
   res.json({ msg: 'Posts works' });
 });
+
+// @route POST api/posts
+// @desc  Create post
+// @access Private
+router.post(
+  '/',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    const { errors, isValid } = validatePostInput(req.body);
+
+    // Check validation
+    if (!isValid) {
+      // If any error, send 400 with errors object
+      return res.status(400).json(errors);
+    }
+
+    const newPost = new Post({
+      text: req.body.text,
+      name: req.body.name,
+      avatar: req.body.avatar,
+      user: req.user.id
+    });
+
+    newPost.save().then(post => res.json(post));
+  }
+);
 
 module.exports = router;
