495621007b90420d040f709b3e1c2c31a2d97676
Your Name <nazik_111@mail.ru>
2018-07-30 17:15:07 +0500

    add redux extension setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+import store from '../src/store';
+
 import Navbar from './components/layout/Navbar';
 import LandingPage from './components/layout/Landing';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+
 //import AboutMe from './components/layout/About-me';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div>
+      <Provider store={store}>
         <Router>
-          <div>
+          <div className="App">
             <Navbar />
             <Route exact path="/" component={LandingPage} />
             <div className="container">
@@ -22,7 +26,7 @@ class App extends Component {
             </div>
           </div>
         </Router>
-      </div>
+      </Provider>
     );
   }
 }
